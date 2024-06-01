2d2ec9d84be74b5bd33c7556ddff0297d1cbef89
xin <102584033+golden-assassin@users.noreply.github.com>
2024-06-02 02:33:34 +0900

    Update pip.js

--- scripts/pip.js
+++ scripts/pip.js
@@ -1,37 +1,29 @@
+
 if (location.host === "www.youtube.com") {
-  let video;
-  let permit = true;
-  let retry = 0;
-  const MAX_RETRY = 5;
-  function input(event) {
-    event.key === 'A' && permit ? togglePiP() : (event.key === 'R' && (permit = !permit));
-  }
   function togglePiP() {
     const video = document.querySelector('video.html5-main-video');
     document.pictureInPictureElement ? document.exitPictureInPicture() : video.requestPictureInPicture();
   }
-  function addPiPButton() {
-    console.log("a")
-  }
-  function waitVideoLoad() {
-    (location.pathname === "/watch" && document.readyState === "complete") ?
-    (video = document.querySelector('video.html5-main-video')) ? addPiPButton() :
-    ((retry < MAX_RETRY) ? (retry++, setTimeout(waitVideoLoad, 500)) : null) : setTimeout(waitVideoLoad, 1000);
+  let permit = true;
+  function input(event) {
+    event.key === 'A' && permit ? togglePiP() : (event.key === 'R' && (permit = !permit));
   }
   window.addEventListener('keydown', input);
   window.addEventListener('beforeunload', () => {
     window.removeEventListener('keydown', input);
   });
-  waitVideoLoad();
-} else {
-  const video = document.querySelector('video');
-let permit = true;
-
-if (video) {
+}
+if (location.host === "www.twitch.tv") {
+  let permit = true;
+  function input(event) {
+    event.key === 'A' && permit ? togglePiP() : (event.key === 'R' && (permit = !permit));
+  }
+  function togglePiP() {
+    const video = document.querySelector('video');
+    document.pictureInPictureElement ? document.exitPictureInPicture() : video.requestPictureInPicture();
+  }
   window.addEventListener('keydown', input);
   window.addEventListener('beforeunload', () => {
     window.removeEventListener('keydown', input);
   });
 }
-}
-
